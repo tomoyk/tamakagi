3b8f4cdc24175538471e9ba234fdb10d6445d6d3
tomoyk <tmyk.kym@gmail.com>
2017-11-16 18:38:30 +0900

    tested theme-customizer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
       <div id="postFooter">
         <p><?php bloginfo('name'); ?><br>
-        &copy;2014 Tokyo Metropolitan Tama High School of Science and Technology Alumni Association.</p>
+        &copy;2014 discription...</p>
       </div><!-- #postFooter -->
     </div><!-- .container -->
   </div><!-- #side-right -->
--- functions.php
+++ functions.php
@@ -15,4 +15,44 @@ function left_widgets_init() {
 }
 add_action( 'widgets_init', 'left_widgets_init' );
 
+/*
+ * Add theme-customizer
+ * * * * * * * * * * * * * * * * * * * * * * */
+function theme_customizer_register($wp_customize){
+
+	$wp_customize->add_section('tamakagi_theme_scheme', array(
+		'title' => '項目名',
+		'priority' => 200,
+	));
+
+	$wp_customize->add_setting('tamakagi_theme_option_text', array(
+		'default' => '',
+		'type'    => 'option',
+		'transport'=> 'refresh',
+	));
+
+	$wp_customize->add_control('tamakagi_theme_origin_text', array(
+		'settings' => 'tamakagi_theme_option_text',
+		'label'    => 'テキスト入力',
+		'section'  => 'tamakagi_theme_scheme',
+		'type'     => 'text',
+	));
+
+}
+add_action('cutomize_register', theme_customizer_register);
+
+function themeCustom(){
+	?>
+
+	<style type="text/css">
+		h1{
+			color: red;
+		}
+	</style>
+
+	<?php
+}
+add_action('wp_head', 'themeCustom');
+
+
 ?>
