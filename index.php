3be63498db35da2a90e2e3d9c6011403824865f4
Tomoyuki KOYAMA <tomoyk@users.noreply.github.com>
2018-06-30 21:14:50 +0900

    Update index.php

--- index.php
+++ index.php
@@ -73,8 +73,7 @@
 
       <?php if(have_posts()): while (have_posts()): the_post(); ?>
       <li><a href="<?php the_permalink(); ?>">
-        <span><?php the_title(); ?></span>
-        <div><?php the_excerpt(); ?></div>
+        <span><?php the_title(); ?></span><div><?php the_excerpt(); ?></div>
       </a></li>
       <?php endwhile; endif; ?>
 
